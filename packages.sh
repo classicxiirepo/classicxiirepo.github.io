78fc075fdf35f121e37d7d344c66c2aabaa9e393
classicxiirepo <classicxii42@gmail.com>
2016-09-25 01:58:10 +0000

    added cxls5

--- packages.sh
+++ packages.sh
@@ -1,3 +1,4 @@
+dpkg-deb -bZgzip projects/LS.Widgets/cxls5 debs
 dpkg-deb -bZgzip projects/LS.Widgets/cxls6 debs
 dpkg-deb -bZgzip projects/LS.Widgets/cxls7 debs
 dpkg-deb -bZgzip projects/LS.Widgets/cxls8 debs
---
+++ projects/LS.Widgets/cxls5/var/mobile/Library/LockHTML/CX.LS 5/Config.js
@@ -0,0 +1,12 @@
+/*
+Configurations
+Credits: Original code by Dacal & Junesiphone
+*/
+
+var Clock = "12h"; // 12h or 24h
+var Lang = "en";  // "en", "fr", "de", "sp", "cz", "nl"
+
+var weathercode = 'USIL0373';  // zip code or weather.com 
+var celsius = false;
+var gpsswitch = false;
+var refreshrate = 10;   // update weather in minutes
