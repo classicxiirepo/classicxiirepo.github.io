f106592641afba875758da3b07785faa4f6aa921
classicxiirepo <classicxii42@gmail.com>
2016-09-25 19:53:50 +0000

    added icon pkg

--- packages.sh
+++ packages.sh
@@ -3,3 +3,4 @@ dpkg-deb -bZgzip projects/LS.Widgets/cxls6 debs
 dpkg-deb -bZgzip projects/LS.Widgets/cxls7 debs
 dpkg-deb -bZgzip projects/LS.Widgets/cxls8 debs
 dpkg-deb -bZgzip projects/LS.Widgets/cxls9 debs
+dpkg-deb -bZgzip projects/CXRepoIcons debs
