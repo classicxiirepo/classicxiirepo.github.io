9f09d4c4b92914c85bbc1d0c43ed91575b44072d
classicxiirepo <classicxii42@gmail.com>
2016-09-27 20:24:33 +0000

    updated cxls5

--- projects/LS.Widgets/cxls5/var/mobile/Library/LockHTML/CX.LS 5/Scripts/Clock.js
+++ projects/LS.Widgets/cxls5/var/mobile/Library/LockHTML/CX.LS 5/Scripts/Clock.js
@@ -1,39 +1,116 @@
-window.addEventListener("load", function() {
-   document.body.style.width='100%';
-   document.body.style.height='100%';
-}, false);
-
-function updateClock() { 
-var currentTime = new Date();
-var currentHours = currentTime.getHours();
-var currentMinutes = currentTime.getMinutes() < 10 ? '0' + currentTime.getMinutes() : currentTime.getMinutes();
-var currentSeconds = currentTime.getSeconds() < 10 ? '0' + currentTime.getSeconds() : currentTime.getSeconds();
-var currentDate = currentTime.getDate() < 10 ? '0' + currentTime.getDate() : currentTime.getDate();
-var currentYear =currentTime.getFullYear();
-timeOfDay = ( currentHours < 12 ) ? "am" : "pm";
-
-if (Clock == "24h"){
-	timeOfDay = "";
-	currentHours = ( currentHours < 10 ? "0" : "" ) + currentHours;
-	currentTimeString = currentHours + ":" + currentMinutes;
-}
-if (Clock == "12h"){
-	currentHours = ( currentHours < 10 ? "0" : "" ) + currentHours;
-	currentHours = ( currentHours > 12 ) ? currentHours - 12 : currentHours;
-	currentHours = ( currentHours == 0 ) ? 12 : currentHours;
-	currentTimeString = currentHours + ":" + currentMinutes;
-}
-document.getElementById("hour").innerHTML = currentHours;
-document.getElementById("minute").innerHTML = currentMinutes;
-document.getElementById("ampm").innerHTML = timeOfDay;
-document.getElementById("weekday").innerHTML = days[currentTime.getDay()];
-document.getElementById("date").innerHTML = currentDate;
-document.getElementById("year").innerHTML = currentYear;
-document.getElementById("month").innerHTML = months[currentTime.getMonth()];
-
-}
-
-function init(){
-updateClock();
-setInterval("updateClock();", 1000);
+/*
+   _,.----.              _,.---._      _,.----.  ,--.-.,-.            ,--.-,  ,-,--.
+ .' .' -   \   _.-.    ,-.' , -  `.  .' .' -   \/==/- |\  \          |==' -|,-.'-  _\
+/==/  ,  ,-' .-,.'|   /==/_,  ,  - \/==/  ,  ,-'|==|_ `/_ /          |==|- /==/_ ,_.'
+|==|-   |  .|==|, |  |==|   .=.     |==|-   |  .|==| ,   /         __|==|, \==\  \
+|==|_   `-' \==|- |  |==|_ : ;=:  - |==|_   `-' \==|-  .|       ,--.-'\=|- |\==\ -\
+|==|   _  , |==|, |  |==| , '='     |==|   _  , |==| _ , \      |==|- |=/ ,|_\==\ ,\
+\==\.       /==|- `-._\==\ -    ,_ /\==\.       /==/  '\  | .=. |==|. /=| -/==/\/ _ |
+ `-.`.___.-'/==/ - , ,/'.='. -   .'  `-.`.___.-'\==\ /\=\.':=; :\==\, `-' /\==\ - , /
+            `--`-----'   `--`--''                `--`       `=`  `--`----'  `--`---'
+** Creator: JunesiPhone
+** Website: http://junesiphone.com/libraries
+** Javascript clocks the easy way.
+** Usage clock({
+          twentyfour : false,
+          padzero : false,
+          refresh : 5000,
+          success: function(clock){
+            $$('#time').innerHTML = clock.hour() + ':' + clock.minute();
+          }
+        });
+*/
+function clock(options) {
+    'use strict';
+    var getTimes = function () {
+            var d = new Date(),
+                funcs = {
+                    hour: function () {
+                        var hour = (options.twentyfour === true) ? d.getHours() : (d.getHours() + 11) % 12 + 1;
+                        hour = (options.padzero === true) ? (hour < 10 ? "0" + hour : " " + hour) : hour;
+                        return hour;
+                    },
+                    rawhour: function () {
+                        return d.getHours();
+                    },
+                    minute: function () {
+                        return (d.getMinutes() < 10) ? "0" + d.getMinutes() : d.getMinutes();
+                    },
+                    rawminute: function () {
+                        return d.getMinutes();
+                    },
+                    am: function () {
+                        return (d.getHours() > 11) ? "pm" : "am";
+                    },
+                    date: function () {
+                        return d.getDate();
+                    },
+                    day: function () {
+                        return d.getDay();
+                    },
+                    month: function () {
+                        return d.getMonth();
+                    },
+                    year: function () {
+                        return d.getFullYear();
+                    },
+                    hourtext: function () {
+                        var hourtxt = (options.twentyfour === true) ? ["Twelve", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen", "Twenty", "Twenty One", "Twenty Two", "Twenty Three", "Twenty Four"] : ["Twelve", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve"];
+                        return hourtxt[this.rawhour()];
+                    },
+                    minuteonetext: function () {
+                        var minuteone = ["o' clock", "o' one", "o' two", "o' three", "o' four", "o' five", "o' six", "o' seven", "o' eight", "o' nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "Sixteen", "Seventeen", "eighteen", "Nineteen", "Twenty", "Twenty", "Twenty", "Twenty", "Twenty", "Twenty", "Twenty", "Twenty", "Twenty", "Twenty", "Thirty", "Thirty", "Thirty", "Thirty", "Thirty", "Thirty", "Thirty", "Thirty", "Thirty", "Thirty", "Forty", "Forty", "Forty", "Forty", "Forty", "Forty", "Forty", "Forty", "Forty", "Forty", "Fifty", "Fifty", "Fifty", "Fifty", "Fifty", "Fifty", "Fifty", "Fifty", "Fifty", "Fifty", "Fifty"];
+                        if (minuteone[this.rawminute()] !== undefined) {
+                            return minuteone[this.rawminute()];
+                        }
+                        return "";
+                    },
+                    minutetwotext: function () {
+                        var minutetwo = ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", ""];
+                        if (minutetwo[this.rawminute()] !== undefined) {
+                            return minutetwo[this.rawminute()];
+                        }
+                        return "";
+                    },
+                    daytext: function () {
+                        var textdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+                        return textdays[this.day()];
+                    },
+                    sdaytext: function () {
+                        var stextdays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+                        return stextdays[this.day()];
+                    },
+                    monthtext: function () {
+                        var textmonth = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+                        return textmonth[this.month()];
+                    },
+                    datetext: function () {
+                        var textdate = ["First", "Second", "Third", "Fourth", "Fifth", "Sixth", "Seventh", "Eighth", "Ninth", "Tenth", "Eleventh", "Twelfth", "Thirteenth", "Fourteenth", "Fifteenth", "Sixteenth", "Seventeenth", "Eightheenth", "Nineteenth", "Twentyith", "TwentyFirst", "TwentySecond", "TwentyThird", 'TwentyFourth', "TwentyFifth", "TwentySixth", "TwentySeventh", "TwentyEight", "TwentyNinth", "Thirtyith", "ThirtyFirst"];
+                        return textdate[this.date() - 1];
+                    },
+                    nth: function (d) {
+                        if (d > 3 && d < 21) {
+                            return 'th';
+                        }
+                        switch (d % 10) {
+                        case 1:
+                            return "st";
+                        case 2:
+                            return "nd";
+                        case 3:
+                            return "rd";
+                        default:
+                            return "th";
+                        }
+                    },
+                    dateplus: function () {
+                        return this.date() + this.nth(Number(this.date()));
+                    }
+                };
+            options.success(funcs);
+            setTimeout(function () {
+                getTimes();
+            }, options.refresh);
+        };
+    getTimes();
 }
