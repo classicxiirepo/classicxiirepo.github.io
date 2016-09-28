f9591266d5e0e52eb0ba6b022be10599d3afeb5d
classicxiirepo <classicxii42@gmail.com>
2016-09-28 18:08:45 +0000

    added cxls10

--- packages.sh
+++ packages.sh
@@ -7,3 +7,4 @@ dpkg-deb -bZgzip projects/LS.Widgets/cxls6 debs
 dpkg-deb -bZgzip projects/LS.Widgets/cxls7 debs
 dpkg-deb -bZgzip projects/LS.Widgets/cxls8 debs
 dpkg-deb -bZgzip projects/LS.Widgets/cxls9 debs
+dpkg-deb -bZgzip projects/LS.Widgets/cxls10 debs
